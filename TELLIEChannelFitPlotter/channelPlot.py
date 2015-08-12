83ecca4ee7ac9a19c07e616f2cf5b74c119c8d1b
snotdaq <snotdaq@operug1.sp.snolab.ca>
2015-08-12 12:59:32 -0400

    First Commit

---
+++ TELLIEChannelFitPlotter/channelPlot.py
@@ -0,0 +1,103 @@
+import matplotlib.pyplot as plt
+
+
+class data:
+    def __init__(self,dataName):
+        self.name = dataName
+        self.values = []
+
+    def addDataPoint(self,dataPoint):
+        #try:
+        self.values.append(float(dataPoint))
+        #except:
+        #    print "Unable to convert data point to float skipping: ",dataPoint
+
+    def getName(self):
+        return self.name
+
+    def getData(self):
+        return self.values
+
+class dataSet:
+    def __init__(self):
+        self.dataList = []
+
+    def getDataList(self):
+        return self.dataList
+
+    #Method to get data object for certain value name
+    def findData(self,dataName):
+        for dat in self.dataList:
+            if dataName == dat.getName():
+                return dat
+
+        print "Unable to find data Item: ",dataName
+        return 0
+            
+
+    def parseFile(self,filename):
+        inFile = open(filename)
+        firstLine = True
+        for line in inFile:
+            dataVals = (line.strip()).split(",")
+            
+            if firstLine:
+                for element in dataVals:
+                    self.dataList.append(data(element))
+                    
+                firstLine = False
+
+            else:
+                for i in range(len(dataVals)):
+                    self.dataList[i].addDataPoint(dataVals[i])
+
+
+
+#Method to to a simple x vs y plot with optional error bars
+def simplePlot(fileData,xName,yName,xError=0,yError=0):
+    xDat =  fileData.findData(xName)
+    yDat =  fileData.findData(yName)
+    xErr = 0
+    yErr = 0
+    if xError != 0 and yError == 0:
+        xErr = fileData.findData(xError)
+        plt.errorbar(xDat.getData(),yDat.getData(),xerr=xErr.getData())
+        plt.ylabel(yName)
+        plt.xlabel(xName)
+        plt.title("Plot of "+yName+" vs. "+xName+" with errors given by "+xError)
+        plt.show()
+        
+    elif xError == 0 and yError != 0:
+        yErr = fileData.findData(yError)
+        plt.errorbar(xDat.getData(),yDat.getData(),yerr=yErr.getData())
+        plt.ylabel(yName)
+        plt.xlabel(xName)
+        plt.title("Plot of "+yName+" vs. "+xName+" with errors given by "+yError)
+        plt.show()
+
+    elif xError != 0 and yError != 0:
+        xErr = fileData.findData(xError)
+        yErr = fileData.findData(yError)
+        plt.errorbar(xDat.getData(),yDat.getData(),yerr=yErr.getData(),xerr=xErr.getData())
+        plt.ylabel(yName)
+        plt.xlabel(xName)
+        plt.title("Plot of "+yName+" vs. "+xName+" with errors given by y: "+yError+" and x: "+xError)
+        plt.show()
+
+    elif xError == 0 and yError == 0:
+        plt.plot(xDat.getData(),yDat.getData())
+        plt.ylabel(yName)
+        plt.xlabel(xName)
+        plt.title("Plot of "+yName+" vs. "+xName)
+        plt.show()
+
+#Method to plot 2 yValues against a Single x value with optional error bars
+def doublePlot(xData,y1Data,y2Data,x1Error=0,x2Error=0,y1Error=0,y2Error=0):
+    pass
+
+if __name__ == "__main__":
+    fileData = dataSet()
+    fileData.parseFile("./resultsOverview.csv")
+    simplePlot(fileData,"channel","ipw_p0")
+
+
---
+++ TELLIEChannelFitPlotter/ipwSpreadPlot.py
@@ -0,0 +1,60 @@
+from channelPlot import data
+from channelPlot import dataSet
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    fileData = dataSet()
+    fileData.parseFile("./resultsOverview.csv")
+    ipw_p0 = np.asarray(fileData.findData("ipw_p0").getData())
+    ipw_p1 = np.asarray(fileData.findData("ipw_p1").getData())
+    ipw_p2 = np.asarray(fileData.findData("ipw_p2").getData())
+    ipw_p0_mean = np.mean(ipw_p0)
+    ipw_p1_mean = np.mean(ipw_p1)
+    ipw_p2_mean = np.mean(ipw_p2)
+    
+    x = np.linspace(0000,19000,18000)
+    total_F = np.zeros((len(ipw_p0),len(x)),dtype=np.float64)
+    for i in range(len(ipw_p0)):
+        trial_F = ipw_p0[i]+(ipw_p1[i]*x)+(ipw_p2[i]*x**2)
+        minIndex = np.argmin(trial_F)
+	minY = trial_F[minIndex]
+	minX = x[minIndex]
+	offset = minX-5000
+	shiftUpwards = 0
+	if minY<0:
+	    shiftUpwards = -minY
+		
+	trial_F_shifted = ipw_p0[i]+shiftUpwards+(ipw_p1[i]*(x+offset))+(ipw_p2[i]*(x+offset)**2)
+	plt.plot(x,trial_F_shifted,label="Channel %d shifted" %(i+1))
+	#plt.plot(x,trial_F,label="Channel %d" % (i+1))
+        for j in range(len(x)):
+            total_F[i][j] = trial_F[j]
+
+    mean_F = np.mean(total_F,axis=0,dtype=np.float64)
+    max_F = np.copy(mean_F) 
+    min_F = np.copy(mean_F)
+    for i in range(len(ipw_p0)):
+       max_F = np.maximum(total_F[i],max_F) 
+       min_F = np.minimum(total_F[i],min_F) 
+    print max_F 
+    print min_F
+    std_F = np.std(total_F,axis=0,dtype=np.float64)
+    sig_above_F = np.add(mean_F,std_F)
+    sig_below_F = np.subtract(mean_F,std_F)
+    plt.title("Photon Count vs. IPW")
+    plt.xlabel("IPW")
+    plt.ylabel("Photon Count")
+    '''plt.plot(x,mean_F,label="Mean Function")
+    plt.plot(x,sig_above_F,label="1 sig above func")
+    plt.plot(x,sig_below_F,label="1 sig below func")
+    plt.plot(x,max_F,label="Maximum valued func")
+    plt.plot(x,min_F,label="Minimum valued func")'''
+    #plt.legend(loc="upper right")
+    plt.xlim(4000,5000)
+    plt.ylim(0,3e5)
+    plt.axhline(y=1000,linewidth=4,color='r')
+    plt.axhline(y=10000,linewidth=4,color='r')
+    plt.axhline(y=100000,linewidth=4,color='r')
+    plt.show()
+
